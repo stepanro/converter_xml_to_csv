8d79f2df92bfb9b3ee745e1584ac809947aca1e7
Stepan Rozhkov <46667510+stepanro@users.noreply.github.com>
2022-11-30 13:59:56 +0000

    some change in main.py

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import argparse
 import re
 from chardet.universaldetector import UniversalDetector
 
+column_name = {
+        'ДатаФайл': '(<ДатаФайл>)(.+)(<\/ДатаФайл>)',
+        'ЛицСч': '(<ЛицСч>)(.+)(<\/ЛицСч>)',
+        'ФИО': '(<ФИО>)(.+)(<\/ФИО>)',
+        'Адрес': '(<Адрес>)(.+)(<\/Адрес>)',
+        'Период': '(<Период>)(.+)(<\/Период>)',
+        'Сумма': '(<Сумма>)(.+)(<\/Сумма>)'
+    }
+
+
 class XmlToStlConverter:
-    def __init__(self):
+    def __init__(self, column_name):
         self.data = dict()
+        self.column_name = column_name
 
     def detect_encoding(self, path_dir):
         with open(file=path_dir, mode='rb') as temp_xml:
@@ -30,11 +41,17 @@ class XmlToStlConverter:
     def recursive_scanner(self):
         with open(file=self.data['path_dir'], mode='r', encoding=self.data['encoding']) as temp_xml:
             for line in temp_xml.readlines():
-                print(line, end=' ')
+                for pattern in self.column_name.values():
+                    answer_regular = re.findall(pattern, line)
+                    if answer_regular:
+                        print(answer_regular[0][1])
+                    else:
+                        pass
 
 
 if __name__ == '__main__':
-    main_converter = XmlToStlConverter()
+    
+    main_converter = XmlToStlConverter(column_name=column_name)
     path_dir = main_converter.arguments_parser()
     main_converter.detect_encoding(path_dir)
     main_converter.recursive_scanner()
