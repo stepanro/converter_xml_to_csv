c6ce49f1b6e132d727d3dccf8e9017ff7dcff42a
Replit user <>
2022-12-06 13:28:26 +0000

    delete cash file

--- converter/converter.py
+++ converter/converter.py
@@ -43,29 +43,22 @@ class XmlToStlConverter:
         return path_input_file
 
     def scanner(self):
-        row_count = 0
-        
         with open(file=self.data['path_input_file'], mode='r', encoding=self.data['encoding']) as temp_xml:
-            count_column_name = len(self.column_name) + 1
+            count_column_name = len(self.column_name)
             for line in temp_xml.readlines():
                 for column_count, pattern in enumerate(self.column_name.values()):
                     answer_regular = re.findall(pattern, line)
                     if answer_regular:
                         with open(file=f"{self.data['path_dir_input_file'] + self.data['name_out_csv_file']}", 
                                   mode='a', encoding=self.data['encoding']) as temp_csv:
-
-                            if column_count + 2 < count_column_name:
+                            
+                            if column_count + 1 < count_column_name:
                                 end_text = ';'
                             else:
                                 end_text = '\n'
-                            if row_count > 0:
-                                temp_csv.write(';;')
                             text = f"{answer_regular[0][1]}{end_text}"
                             temp_csv.write(text)
-                        row_count += 1   
                     else:
                         pass
-            else:
-                row_count = 0
 
 
